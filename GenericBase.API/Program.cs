ca9b811475ea08f2a8f343db02c0192d1af78255
JamesMaxwel <jamesmaxwelquadrocoelho@gmail.com>
2023-10-14 16:03:57 -0300

    Created Project Layers

---
+++ GenericBase.API/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//IOC
+//builder.Services.AddServiceLayer(builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseStaticFiles();
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.MapControllers();
+
+//IOC 
+//app.AddBuildLayer();
+
+app.Run();
