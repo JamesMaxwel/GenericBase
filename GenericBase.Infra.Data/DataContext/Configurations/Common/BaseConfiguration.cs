9a55f89bec7565d999dde75e3898de42e1db69cf
JamesMaxwel <jamesmaxwelquadrocoelho@gmail.com>
2023-10-16 10:36:14 -0300

    Created DbContext

---
+++ GenericBase.Infra.Data/DataContext/Configurations/Common/BaseConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using GenericBase.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GenericBase.Infra.Data.DataContext.Configurations.Common
+{
+    public abstract class BaseConfiguration<T> : IEntityTypeConfiguration<T> where T : class, IBase
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
---
+++ GenericBase.Infra.Data/DataContext/MyDbContext.cs
@@ -0,0 +1,32 @@
+﻿using GenericBase.Domain.Entities;
+using GenericBase.Domain.Entities.Account;
+using Microsoft.EntityFrameworkCore;
+
+namespace GenericBase.Infra.Data.DataContext
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Permission>? Permissions { get; set; }
+        public DbSet<Role>? Roles { get; set; }
+        public DbSet<Customer>? Customers { get; set; }
+        public DbSet<Product>? Products { get; set; }
+        public DbSet<Category>? ProductCategories { get; set; }
+        public DbSet<Order>? Orders { get; set; }
+        public DbSet<OrderItem>? OrderDetails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSnakeCaseNamingConvention();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        }
+    }
+}
